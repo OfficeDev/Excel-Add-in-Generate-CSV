0f3c862471592fa1e4a92444486689c05423781a
John Austin <johnau@microsoft.com>
2015-10-22 08:48:27 -0700

    add service select drop down

--- Code Editor project/Home.js
+++ Code Editor project/Home.js
@@ -1,7 +1,7 @@
 ﻿/* Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. 
     See full license at the bottom of this file. */
 
-/// <reference path="../App.js" />
+
 
 (function () {
     "use strict";
